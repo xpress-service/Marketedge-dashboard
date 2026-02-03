125d36fd3c5f2f5aa1759b93b7bd391b7d7ad3e7
xpress-service <servixpress247@gamil.com>
2026-02-03 12:55:56 +0100

    added live url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://marketedge-server.onrender.com';
 
 export const fetchAllLocations = async () => {
   try {
